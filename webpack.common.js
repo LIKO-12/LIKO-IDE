2fb369f7eddeb5ce2c64b01bd5d29308d527ca79
Rami Sabbagh <rami.sab07@gmail.com>
2023-02-16 21:55:09 +0300

    Setup production webpack configuration

--- webpack.common.js
+++ webpack.common.js
@@ -4,20 +4,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 // TODO: Tree shaking.
-// TODO: Chunk splitting, avoid unnecessary download of monaco on each update.
-// TODO: Provide production configuration.
-// TODO: Provide CSS support.
 
 /**
  * @type {import('webpack').Configuration & { devServer: import('webpack-dev-server').Configuration }}
  */
 module.exports = {
-    mode: 'development',
     entry: './src/main.ts',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'LIKO-12 WebIDE'
@@ -48,10 +40,19 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
+        filename: '[name].[chunkhash].js',
         clean: true,
     },
     optimization: {
         runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
     },
 };
