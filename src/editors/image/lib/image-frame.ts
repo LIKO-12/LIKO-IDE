73343e5980b233f3f38c0f70a6799f190c138d7f
Rami Sabbagh <rami.sab07@gmail.com>
2023-06-30 16:32:14 +0300

    Make image frames changes observable

--- src/editors/image/lib/image-frame.ts
+++ src/editors/image/lib/image-frame.ts
@@ -1,21 +1,53 @@
+type ChangeObserver = (frame: ImageFrame) => void;
+
 export class ImageFrame {
     /**
-     * Each pixel is 4-bytes, but only the first byte represents the color value.
-     * 
-     * The other 3-bytes for each pixel are there to allow uploading the image to WebGL more straight forward.
+     * Follows the same convention of `data` but allows read-write.
      */
-    public readonly data = new Uint8ClampedArray(4 * this.width * this.height);
+    private readonly rwData = new Uint8ClampedArray(4 * this.width * this.height);
+
+    private observers: ChangeObserver[] = [];
 
     constructor(
         public readonly width: number,
         public readonly height: number,
     ) { }
 
+    /**
+     * Each pixel is 4-bytes, but only the first byte represents the color value.
+     * 
+     * The other 3-bytes for each pixel are there to allow uploading the image to WebGL more straight forward.
+     */
+    get data(): Readonly<Uint8ClampedArray> {
+        return this.rwData;
+    }
+
     setPixel(x: number, y: number, color: number): void {
-        this.data[4 * (y * this.width + x)] = color;
+        this.rwData[4 * (y * this.width + x)] = color;
+        this.notify();
     }
 
     getPixel(x: number, y: number): number {
-        return this.data[4 * (y * this.width * x)];
+        return this.rwData[4 * (y * this.width * x)];
+    }
+
+    observe(observer: ChangeObserver): void {
+        if (this.observers.includes(observer)) return;
+        this.observers.push(observer);
+    }
+
+    /**
+     * Unregister an observer.
+     */
+    miss(observer: ChangeObserver): void {
+        this.observers = this.observers.filter(value => value !== observer);
+    }
+
+    /**
+     * Notifies the observers of changes.
+     */
+    private notify(): void {
+        for (const observer of this.observers)
+            observer(this);
     }
 }
